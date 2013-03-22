d1b715c5752cfcf0a1b201aa23ceac201d815849
John Kercheval <kercheval@gmail.com>
2013-03-22 15:56:13 -0700

    Add a simple main for testing

---
+++ src/main/java/org/kercheval/main/Main.java
@@ -0,0 +1,35 @@
+package org.kercheval.main;
+
+import org.kercheval.statistics.Counter;
+import org.kercheval.statistics.Timer;
+import org.kercheval.statistics.Timer.TimerState;
+
+public class Main
+{
+    //
+    // Simple main to start a VM and create a few timers and counters.
+    // This is a testing main.
+    //
+    public static void main(final String[] args) throws InterruptedException
+    {
+        final Timer timerParent = Timer.getTimer("Parent");
+        final Timer timerNormal = Timer.getTimer("Timer", timerParent);
+
+        final Counter counterParent = Counter.getCounter("Parent");
+        final Counter counterNormal = Counter.getCounter("Counter", counterParent);
+
+        while (true) {
+            final TimerState timerState = timerNormal.start();
+            try {
+                counterNormal.increment(1);
+                Thread.sleep(100 + counterNormal.getCount());
+            }
+            finally
+            {
+                timerState.stop();
+                counterParent.increment(1);
+            }
+        }
+    }
+
+}
